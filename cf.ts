d064de05c1dee4ef8a9e1661b99815e0f45aaf8f
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-11-27 14:27:33 +0900

    Update cf.ts

--- cf.ts
+++ cf.ts
@@ -1,1 +1,31 @@
+type Chemical = string;
+type Molecule = string;
 
+type SymbolMark = Chemical & Molecule;
+type OnEq = {
+	"left": SymbolMark[],
+	"right": SymbolMark[]
+}
+
+type StrEq = `${string} = ${string}`
+
+const questions: OnEq[] = [
+	{
+		"left": ["C", "O_2"],
+		"right": ["CO2"]
+	}
+]
+
+function randomPick<T = unknown>(array: T[]): T {
+	return array[Math.floor(Math.random() * array.length)]
+}
+
+function createQuestion(): OnEq {
+	return randomPick<OnEq>(questions);
+}
+
+function parseEq(question: OnEq): StrEq {
+	return `${question["left"].join(" + ")} = ${question["right"].join(" + ")}`;
+}
+
+console.log(parseEq(createQuestion()))
